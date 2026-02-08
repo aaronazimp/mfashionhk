dd0f3c809a705f19a1fc6955604ff39765e65d4c
aaronazimp <aaron.c@azimp.ltd>
2026-02-08 21:03:12 +0000

    fix(flash-sale): wrap client component in Suspense to satisfy useSearchParams CSR bailout

--- app/flash-sale/page.tsx
+++ app/flash-sale/page.tsx
@@ -1,6 +1,11 @@
+import React from 'react'
 import FlashSaleClient from './FlashSaleClient'
 
 export default function Page() {
-  return <FlashSaleClient />
+  return (
+    <React.Suspense fallback={<div className="min-h-screen bg-black" />}>
+      <FlashSaleClient />
+    </React.Suspense>
+  )
 }
 
